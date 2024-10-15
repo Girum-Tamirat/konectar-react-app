1b331e97e587ef0f6fd8ac5f3f0ffaab1c09c6d1
Girum-Tamirat <girumtamirat10@gmail.com>
2024-10-15 19:14:43 +0300

    Add HeroSection and CallToAction components with initial styling

---
+++ src/components/CallToAction.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function CallToAction() {
+    return (
+<button className="bg-green-600 text-white text-lg py-2 px-6 rounded-lg hover:bg-green-700 transition duration-300">
+            Join Waitlist
+        </button>    );
+};
