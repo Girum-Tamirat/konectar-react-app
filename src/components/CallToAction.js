b1368343e5a84a0c6361d423d79fb906ec9c1e51
Girum-Tamirat <girumtamirat10@gmail.com>
2024-10-15 23:09:00 +0300

    Additional fixes on Hero-Section

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function CallToAction() {
     return (
-<button className="bg-green-600 text-white text-lg py-2 px-6 rounded-lg hover:bg-green-700 transition duration-300">
+<button className="bg-green-600 text-white w-48 text-lg py-2 px-6 rounded-lg hover:bg-green-700 transition duration-300">
             Join Waitlist
         </button>    );
 };
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -5,16 +5,16 @@ import CallToAction from './CallToAction.js';
 export default function HeroSection() {
     return (
         <div className="hero-section flex flex-col items-center md:flex-row md:items-center bg-gray-50 py-10">
-            <div className="md:w-1/2 text-center md:text-left px-4">
-                <h1 className="text-4xl font-bold text-gray-900 mb-4">Unlock Markets and Increase Profits</h1>
-                <p className="text-lg text-gray-600 mb-6">
-                    Skip multiple middlemen and sell directly to businesses like: <br />
+            <div className="md:w-1/2 text-center md:text-left px-7 md:ml-8">
+                <h1 className="text-6xl font-semibold text-gray-900 mb-5">Unlock Markets and Increase Profits</h1>
+                <p className="text-lg font-medium max-w-lg mb-6">
+                    Skip multiple middlemen and sell directly to businesses like:<br />
                     Supermarkets, Quick Service, Restaurants, Hotels, and Food Processors in One Click!
                 </p>
                 <CallToAction />
             </div>
-            <div className="md:w-1/2 px-4 mt-6 md:mt-0">
-                <img src={heroImage} alt="Farmer holding vegetables" className="w-full rounded-lg shadow-lg" />
+            <div className="md:w-1/2 px-12 py-12 m-5 mr-11 md:mt-0">
+                <img src={heroImage} alt="Farmer holding vegetables" className="w-full rounded-2xl shadow-lg" />
             </div>
         </div>
     );
